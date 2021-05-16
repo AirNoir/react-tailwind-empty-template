260063bb618db31747e7bfcbd941d2f31dabfeb7
williams <williams@voicetube.com>
2021-05-16 17:01:55 +0800

    init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HeaderBar from '@/components/layout/HeaderBar.tsx'
+
+function App() {
+  return (
+    <div className="App">
+        <HeaderBar />
+    </div>
+  );
+}
+
+export default App;
